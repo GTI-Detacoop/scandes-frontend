7cfeb57b6de630645779c6ce52dbadd05ce39de4
fipaniagua <fipaniagua@uc.cl>
2025-04-22 16:43:57 -0400

    :lipstick: add vuetify config

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import vuetify from './plugins/vuetify.ts'
+
+
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
