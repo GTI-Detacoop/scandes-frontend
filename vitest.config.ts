ba73a0581f3ef2ac775752b2989bf5b4d056e2ad
fipaniagua <fipaniagua@uc.cl>
2025-05-26 12:44:00 -0400

    :white_check_mark: update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,22 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html', 'lcov', 'cobertura', 'json-summary'],
+        reportsDirectory: './coverage',
+        exclude: [
+          'coverage/**',
+          'dist/**',
+          '**/[.]**',
+          'packages/*/test?(s)/**',
+          '**/*.d.ts',
+          '**/virtual:*',
+          '**/__mocks__/*',
+          '**/node_modules/**',
+          'e2e/**'
+        ]
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
